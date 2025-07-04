0c9de5f68e5fc5cecbedbd7fede3b245363ce4c8
Madhu Pandey <madhupandey29052004@gmail.com>
2025-07-04 17:58:36 +0530

    fix

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL,
+    baseUrl:'https:/adorable-gentleness-production.up.railway.app',
     prepareHeaders: async (headers) => {
       try {
         const userInfo = Cookies.get('userInfo');
